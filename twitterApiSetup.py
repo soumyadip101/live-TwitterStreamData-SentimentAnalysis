97e47a0116bb82a7b4e17f3141309417ef8cd809
Soumyadip Mitra <soumyadip.mitra11@gmail.com>
2019-07-04 01:45:41 +0530

    Twitter API and search words setup file.
    This file can be modified as per needs of the user. Replace <consumer_key>, <consumer_secret>, <app_key> and <app_secret> with your own generated keys.

---
+++ twitterApiSetup.py
@@ -0,0 +1,10 @@
+CONSUMER_KEY = <consumer_key>
+CONSUMER_SECRET = <consumer_secret>
+TWITTER_APP_KEY = <app_key>
+TWITTER_APP_SECRET = <app_secret>
+TRACK_TERMS = ["narendra modi", "BJP", "modi"]
+
+try:
+    from private import *
+except Exception:
+    pass
